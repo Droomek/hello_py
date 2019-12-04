76d282989ceb230277fc190a1c929397409f843e
Lucas Carlson <lucascrlsn@gmail.com>
2019-12-04 09:33:34 -0600

    Add files via upload

--- password_manager/ledger.py
+++ password_manager/ledger.py
@@ -3,37 +3,46 @@
 # Developed By: Lucas Carlson: https://github.com/lucascrlsn
 
 from tkinter import *
-from tkinter import ttk
+import tkinter as ttk
 import ledger_bk
 import tkinter.messagebox
 import datetime
 import time
-from time import strftime
+import pyperclip
+from time import strftime, ctime, gmtime
 
 window = Tk()
 window.title("Password Manager")
 window.geometry('800x400')
-now_date = str(datetime.date.today().strftime('%d%b%y').upper())
-now_hour = str(time.localtime().tm_hour)
-now_min = str(time.localtime().tm_min)
+# highlight color = box around GUI
+window.config(bg='light grey', highlightcolor='red', highlightthickness=5, highlightbackground='black', relief='sunken',
+              borderwidth=5, pady=8)
+toolbar = Frame(window, bg='black')
 
-# def get_date():
-#     # Prints date for status bar
-#     print('Record added' + ' ' + now_date + ' ' + now_hour + now_min)
+
+def copy_password():
+    # Copies password from the GUI
+    p = e3.get()  # Current password in e3 password entry widget
+    n = e1.get()  # Current name off account password belongs to in e1 entry widget
+    pyperclip.copy(p)
+    print(datetime.datetime.strptime(time.ctime(), "%a %b %d %H:%M:%S %Y")), print(
+        ' ' + '|' + ' ' + 'User copied the' + ' ' + n + ' ' + 'account password to the clipboard')
 
 
 def view_command():
     lb.delete(0,END)
+    lb_2.delete(0,END)
     for row in ledger_bk.viewall():
         lb.insert(END,row)
-    print(now_date + ' ' + now_hour + now_min + ':' + ' ' + 'User viewed all records')
+    print(datetime.datetime.strptime(time.ctime(), "%a %b %d %H:%M:%S %Y")), print(' ' + '|' + ' ' + 'User viewed all records')
 
 
 def search_command():
     lb.delete(0,END)
     for row in ledger_bk.search(name=name.get(),user=user.get(),password=password.get(),category=category.get(),url=url.get()):
         lb.insert(END,row)
-    print(now_date + ' ' + now_hour + now_min + ':' + ' ' + 'User searched records')
+    print(datetime.datetime.strptime(time.ctime(), "%a %b %d %H:%M:%S %Y")), print(' ' + '|' + ' ' +
+                                                                                   'User searched records')
 
 
 def add_command():
@@ -47,7 +56,8 @@ def add_command():
     e3.delete(0,END)
     e4.delete(0,END)
     e5.delete(0,END)
-    print(now_date + ' ' + now_hour + now_min + ':' + ' ' + 'User added a record')
+    print(datetime.datetime.strptime(time.ctime(), "%a %b %d %H:%M:%S %Y")), print(' ' + '|' + ' ' +
+                                                                                   'User added a record')
 
 
 def get_selected_row(event):
@@ -95,15 +105,21 @@ def get_selected_metadata(event):
 def update_command():
     ledger_bk.update(selected_tuple[0],name.get(),user.get(),password.get(),category.get(),url.get())
     view_command()
-    print(now_date + ' ' + now_hour + now_min + ':' + ' ' + 'User updated a record')
+    r = name.get()
+    print(datetime.datetime.strptime(time.ctime(), "%a %b %d %H:%M:%S %Y")), print(' ' + '|' + ' ' + 'User updated the'
+                                                                                   + r + 'record')
+
 
 def delete_command():
     # lb.delete(END,get_selected_row.selected_tuple)
     tkinter.messagebox.askyesno(title='Alert', message='''Are you sure you want to delete this record? 
     This action cannot be undone.''')
     ledger_bk.delete(selected_tuple[0])
-    view_command()
-    print(now_date + ' ' + now_hour + now_min + ':' + ' ' + 'User deleted a record')
+    print(datetime.datetime.strptime(time.ctime(), "%a %b %d %H:%M:%S %Y")), print(' ' + '|' + ' ' +
+                                                                                   'User deleted a record')
+    lb.delete(0, END)
+    for row in ledger_bk.viewall():
+        lb.insert(END, row)
 
 
 def clear_command():
@@ -114,7 +130,8 @@ def clear_command():
     e3.delete(0,END)
     e4.delete(0,END)
     e5.delete(0,END)
-    print(now_date + ' ' + now_hour + now_min + ':' + ' ' + 'User cleared all records from GUI')
+    print(datetime.datetime.strptime(time.ctime(), "%a %b %d %H:%M:%S %Y")), print(' ' + '|' + ' ' +
+                                                                                   'User cleared all records')
 
 
 l1 = Label(window,text="Name")
@@ -136,8 +153,9 @@ user = StringVar()
 e2 = ttk.Entry(window,textvariable=user,width=25)
 e2.grid(row=1,column=2,columnspan=10, sticky=W)
 
+# show='*' in line e3 to show *** when password entered
 password = StringVar()
-e3 = ttk.Entry(window,textvariable=password,width=25)
+e3 = ttk.Entry(window,textvariable=password, width=25, show='*')
 e3.grid(row=2,column=2,columnspan=10, sticky=W)
 
 category = StringVar()
@@ -166,9 +184,13 @@ b5.grid(row=10,column=0, padx=10)
 b6 = ttk.Button(window,text="Clear All",width=12,command=clear_command)
 b6.grid(row=11,column=0, padx=10)
 
-b7 = ttk.Button(window,text="Close",width=12, command=window.destroy)
-b7.grid(row=12,column=0, padx=10)
+b7 = ttk.Button(window,text="Close", width=12, command=window.destroy)
+b7.grid(row=12, column=0, padx=10)
 
+# Str Var Action Buttons, needs better grid management before IMP
+
+b8 = ttk.Button(window,text="Copy",width=12, command=copy_password)
+b8.grid(row=2,column=3, padx=10)
 
 # # Combobox for email service
 # email_service_chooser = ttk.Combobox(window, width="10", values=['gmail', 'outlook', 'aol', 'icloud', 'mail'])
@@ -183,17 +205,17 @@ b7.grid(row=12,column=0, padx=10)
 # # End Combobox
 
 # listbox_1
-lb = Listbox(window,height=10,width=30)
+lb = Listbox(window,height=10, width=30, highlightcolor='black', highlightthickness=3, exportselection=False)
 lb.grid(row=6,column=2,rowspan=8, pady=10)
 
 sb = Scrollbar(window)
-sb.grid(row=6,column=1,rowspan=8, padx=5)
+sb.grid(row=7,column=1,rowspan=8, padx=5)
 lb.configure(yscrollcommand=sb.set)
 sb.configure(command=lb.yview)
 
 # listbox_2
-lb_2 = Listbox(window,height=10,width=30)
-lb_2.grid(row=6,column=4,columnspan=6,rowspan=8, pady=10)
+lb_2 = Listbox(window, height=10, width=30, highlightcolor='black', highlightthickness=3, exportselection=False)
+lb_2.grid(row=6,column=3,columnspan=6,rowspan=8, pady=10)
 
 lb.bind('<<ListboxSelect>>',get_selected_row)
 window.mainloop()
